8a7c0551dc1178719009403d74a9e46733059a96
deristnoor <deris.t.noor@gmail.com>
2020-12-07 10:17:30 +0700

    1st

--- project/Dr_Semmelweis_and_the_Discovery_of_Handwashing/notebook.py
+++
@@ -1,8 +0,0 @@
-# importing modules
-import pandas as pd
-
-# Read datasets/yearly_deaths_by_clinic.csv into yearly
-yearly = pd.read_csv("datasets/yearly_deaths_by_clinic.csv")
-
-# Print out yearly
-print(yearly)
